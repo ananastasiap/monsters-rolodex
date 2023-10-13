8a5813aa00737a4cad6b6339e4de62d5fd306658
ananastasiap <anestheziia@gmail.com>
2023-10-13 23:05:22 +0300

    refactor: rewrite App using ts

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,11 +1,12 @@
-import { ChangeEventHandler } from 'react';
+// import { ChangeEventHandler } from 'react';
+import { ChangeEvent } from 'react';
 
 import './search-box.styles.css';
 
 type SearchBoxProps = {
   className: string;
   placeholder?: string;
-  onChangeHandler: ChangeEventHandler<HTMLInputElement>;
+  onChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
 const SearchBox = ({
